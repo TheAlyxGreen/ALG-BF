b37d7bcedf8d4128afdae0a4d87d3a510413f761
TheAlyxGreen <3075793+TheAlyxGreen@users.noreply.github.com>
2023-05-25 14:44:49 -0500

    Fixed a bug where the last cell was not being shown

--- src/components/output-view/memory-viewer.tsx
+++ src/components/output-view/memory-viewer.tsx
@@ -8,15 +8,15 @@ export type MemoryViewerProps = {
 export default function MemoryViewer(props: MemoryViewerProps): React.ReactElement {
 
 	let elements: JSX.Element[] = new Array(props.maxCells);
-	for (let i=0;i<props.maxCells;i++){
-		elements[i] = <div id={"memCell-"+i} className={"memCell"}>
-			<span id={"memCell-label-"+i} className={"memCell-content memCell-label"}>
+	for (let i = 0; i < props.maxCells + 1; i++) {
+		elements[i] = <div id={"memCell-" + i} className={"memCell"}>
+			<span id={"memCell-label-" + i} className={"memCell-content memCell-label"}>
 				{i}
 			</span>
-			<span id={"memCell-int-"+i} className={"memCell-content memCell-int"}>
+			<span id={"memCell-int-" + i} className={"memCell-content memCell-int"}>
 				{props.memory[i]}
 			</span>
-			<span id={"memCell-char-"+i} className={"memCell-content memCell-char"}>
+			<span id={"memCell-char-" + i} className={"memCell-content memCell-char"}>
 				"{String.fromCharCode(props.memory[i])}"
 			</span>
 		</div>
